d32f416ce1abb8107b4aab8e62b4a40dc276cc66
larads <dasilval.mariana@gmail.com>
2025-05-25 13:18:11 -0400

    Add FilterStatus enum for item filtering, update tsconfig paths, and create components for Input, Filter, Item, Button, and StatusIcon with styles.

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,11 +1,67 @@
-import { Text, View } from "react-native"
+import { Image, TouchableOpacity, View, Text, ScrollView, FlatList } from "react-native"
 
 import { styles } from "./styles"
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
+import { Item } from "@/components/Item"
 
-export default function App() {
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "1 pacote de café"
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "3 pacotes de macarrão"
+  },
+  {
+    id: "3",
+    status: FilterStatus.PENDING,
+    description: "3 cebolas"
+  },
+]
+
+export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+      <View style={styles.form}
+      >
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={ITEMS}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item}
+              onStatus={() => console.log("mudar o status")}
+              onRemove={() => console.log("remover")}
+            />
+          )}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
+        />
+      </View>
     </View>
   )
 }
